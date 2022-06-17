e3f53affbaf5e137ebb697446aefd28489acf01e
Allifian#14 <78087246+fianbro@users.noreply.github.com>
2022-06-17 14:26:13 +0700

    Update donasi.js

--- plugins/donasi.js
+++ plugins/donasi.js
@@ -1,6 +1,10 @@
 let handler =  m => m.reply(`
 ╭─「 Donasi 」
 │ • ${global.donasi}
+│ • *Rincian Fungsi Program* iki
+│   />Stiker Maker
+│   />Download video Fb, Yt , Tt
+│  _Setidak e iku tok seng tak angget berguna *fitur* e_
 ╰────
 `.trim()) // Tambah sendiri kalo mau
 handler.help = ['donasi']
